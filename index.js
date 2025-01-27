f7e08bb12976fa1d51e614a13df74592fc4e5956
Jansen <jan.mangu.fernan@gmail.com>
2025-01-28 00:25:28 +0800

    database connected

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+const dbPromise = open({
+    filename: './database.db',
+    driver: sqlite3.Database
+});
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.set('views', join(__dirname, 'views'));
 
-// app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const db = await dbPromise;
+    const home = await db.all('SELECT * FROM Chairs');
     res.render('home', { 
-        title: 'Home' 
+        title: 'Home',
+        home
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+const setup = async () => {
+    const db = await dbPromise;
+    await db.migrate({ migrationsPath: join(__dirname, 'migrations') });
+    app.listen(3000, () => {
+        console.log('Server is running on http://localhost:3000');
+    });
+};
+
+setup();
