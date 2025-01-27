43d68c5546956ebe89eff2ebb4159a2254771504
Jansen <jan.mangu.fernan@gmail.com>
2025-01-27 22:08:12 +0800

    handlebars are now connected

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
+import express from 'express';
+import { engine } from 'express-handlebars';
+
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.render('home', { title: 'Home' });
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on http://localhost:3000');
 });
