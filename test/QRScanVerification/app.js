1ca02fc133653e2048d394389ef09f9e416edb7e
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-01-28 10:10:27 +0800

    working qrcode image

--- test/QRScanVerification/app.js
+++ test/QRScanVerification/app.js
@@ -18,22 +18,7 @@ app.get('/', (req, res) => {
     res.sendFile("static/html/index.html", { root: __dirname });
 });
 
-app.get("/generate-qr", (req, res) => {
-    const data = req.query.text;
-    const url = `https://example.com/${data}`;
 
-    qrcode.toFile(`qr/${data}.png`, url, {
-        color: {
-            dark: '#000000',
-            light: '#ffffff'
-        },
-        width: 500,
-        margin: 2
-    }, (err, url) => {
-        if (err) return res.status(500).send("Error generating QR code");
-        res.json({ message: "QR code generated", url: `qr/${data}.png` });
-    });
-});
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
