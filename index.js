4342629d13f5f114c44002a896ef45ecd5d6016f
Jansen <jan.mangu.fernan@gmail.com>
2025-01-29 09:54:11 +0800

    chair to student data

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {  
     const db = await dbPromise;
-    const home = await db.all('SELECT * FROM Chair');
+    const home = await db.all('SELECT * FROM StudentData');
     res.render('home', { 
         title: 'Home',
         css: 'css/home.css',
@@ -37,7 +37,7 @@ app.get('/searchStudentData', async (req, res) => {
     const { q } = req.query;
     const db = await dbPromise;
     const data = await db.all(
-        'SELECT * FROM Chair WHERE student_id LIKE ?', [`%${q}%`]);
+        'SELECT * FROM StudentData WHERE student_id LIKE ?', [`%${q}%`]);
     res.json(data);
 });
 
