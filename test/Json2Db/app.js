cff09f40605bdf4639dc6a1afcb090c307c383bf
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-01-29 23:38:27 +0800

    added JSON to Database sample code

--- test/Json2Db/app.js
+++ test/Json2Db/app.js
@@ -38,9 +38,15 @@ app.get('/', (req, res) => {
     res.sendFile("static/html/index.html", { root: __dirname });
 });
 
-app.post("/upload", upload.single("file"), (req, res) => {
+// https://gist.github.com/christopherscott/2782634
+function getJsDateFromExcel(excelDate) {
+    return new Date((excelDate - (25567 + 1))*86400*1000).toISOString().split("T")[0];
+}
+
+app.post("/upload", upload.single("file"), async (req, res) => {
     if (!req.file) return res.status(400).send("Please upload a file");
     try {
+        const db = await dbPromise;
         const filePath = path.resolve(req.file.path);
         
         // readFile doesnt work for some reason, so I buffer read it instead
@@ -50,11 +56,44 @@ app.post("/upload", upload.single("file"), (req, res) => {
         const workbook = xlsx.read(fileBuffer, { type: "buffer" });
 
         const sheetName = workbook.SheetNames[0];
-        const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-        
-        // res.json(sheetData);
+        var sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
-        // code here for inserting data to database
+        // clean json data
+        sheetData = sheetData.map((row) => {
+            return {
+                segment: row["Segment"],
+                county: row["Country"],
+                product: row[" Product "]?.trim(),
+                discount_band: row[" Discount Band "]?.trim(),
+                units_sold: parseFloat(row["Units Sold"]),
+                manufacturing_price: parseFloat(row[" Manufacturing Price "]),
+                sale_price: parseFloat(row[" Sale Price "]),
+                gross_sales: parseFloat(row[" Gross Sales "]),
+                discounts: parseFloat(row[" Discounts "]),
+                sales: parseFloat(row["  Sales "]),
+                cogs: parseFloat(row[" COGS "]),
+                profit: parseFloat(row[" Profit "]),
+                date: getJsDateFromExcel(row["Date"]),
+                month_number: parseInt(row["Month Number"]),
+                month_name: row[" Month Name "]?.trim(),
+                year: parseInt(row["Year"])
+            }
+        });
+
+        // insert data to db
+        const query = `
+            INSERT INTO FinancialSample (
+                segment, country, product, discount_band, units_sold, manufacturing_price, sale_price, gross_sales, discounts, sales, cogs, profit, date, month_number, month_name, year
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        `;
+        
+        // query db
+        for(const row of sheetData) {
+            await db.run(query, Object.values(row));
+        }
+        
+        console.log("File uploaded to database successfully");
+        res.status(200).send("File uploaded to database successfully");
     } catch (error) {
         res.status(500).send(error.message);
     }
--- test/Json2Db/migrations/001-initial-schema.sql
+++ test/Json2Db/migrations/001-initial-schema.sql
@@ -1,5 +1,20 @@
 -- Up
-CREATE TABLE Example (
+CREATE TABLE FinancialSample (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    segment TEXT NOT NULL,
+    country TEXT NOT NULL,
+    product TEXT NOT NULL,
+    discount_band TEXT NOT NULL,
+    units_sold REAL NOT NULL,
+    manufacturing_price REAL NOT NULL,
+    sale_price REAL NOT NULL,
+    gross_sales REAL NOT NULL,
+    discounts REAL NOT NULL,
+    sales REAL NOT NULL,
+    cogs REAL NOT NULL,
+    profit REAL NOT NULL,
+    date TEXT NOT NULL,
+    month_number INTEGER NOT NULL,
+    month_name TEXT NOT NULL,
+    year INTEGER NOT NULL
 );
