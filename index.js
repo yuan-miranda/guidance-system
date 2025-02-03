3b0338b422a6b2402803446fdc279bc3b773e2ec
Jansen <jan.mangu.fernan@gmail.com>
2025-02-03 14:33:05 +0800

    reverted the port of the project

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3001
+const port = 3000
 const upload = multer();
 
 const dbPromise = open({
